a8cf27ed43337366d73717c3e4c0765c464d98ca
Artem Konusheusky <konusevskiart@gmail.com>
2023-07-08 23:14:01 +0300

    fix: delete private api key log

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log(process.env.REACT_APP_API_KEY)
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
